56c5a5c342a0def2b1dd1fbfddffee52ef8da6ef
vice <vice@vice.com>
2025-08-31 01:51:03 +0300

    add filter to module

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash_bootstrap_components as dbc
-from dash import Dash, dcc
-from header import header
+from dash import Dash
+from header import header, filter
 
 app = Dash(
     external_stylesheets=[dbc.themes.DARKLY],
@@ -14,76 +14,7 @@ app.title = "NYC Vehicle Collisions Analysis"
 
 app.layout = (
     dbc.Container(
-        children=[
-            header(),
-            dbc.Row(
-                dbc.Col(
-                    dbc.Card(
-                        [
-                            dbc.CardHeader(
-                                "Filters", className="border-0 bg-transparent"
-                            ),
-                            dbc.CardBody(
-                                dbc.Row(
-                                    [
-                                        dbc.Col(
-                                            dbc.Checklist(
-                                                id="borough-checklist",
-                                                options=[
-                                                    {"label": b, "value": b.upper()}
-                                                    for b in [
-                                                        "Queens",
-                                                        "Brooklyn",
-                                                        "Manhattan",
-                                                        "Bronx",
-                                                        "Staten Island",
-                                                    ]
-                                                ],
-                                                value=[
-                                                    "QUEENS",
-                                                    "BROOKLYN",
-                                                    "MANHATTAN",
-                                                    "BRONX",
-                                                    "STATEN ISLAND",
-                                                ],
-                                                inline=True,
-                                                switch=True,
-                                            ),
-                                            md=6,xs=12
-                                        ),
-                                        dbc.Col(
-                                            dcc.RangeSlider(
-                                                id="year-range",
-                                                min=2018,
-                                                max=2026,
-                                                step=1,
-                                                value=[2018, 2026],
-                                                marks={year: str(year) if year % 2 == 0 else "" for year in range(2018, 2027)},
-                                                tooltip={
-                                                    "placement": "bottom",
-                                                    "always_visible": True,
-                                                    "style": {"color": "white"},
-                                                },
-                                                allowCross=False,
-                                                updatemode="mouseup",
-                                                pushable=1,
-                                                included=True,
-                                            ),
-                                            md=6,xs=12
-                                        ),
-                                    ],
-                                    className="gy-2"
-                                )
-                            ),
-                        ],
-                        className="border-0 bg-transparent",
-                    ),
-                    width=10,
-                ),
-                justify="center",
-                className="shadow-sm",
-            ),
-        ],
+        children=[header(), filter()],
         fluid=True,
     ),
 )
--- header.py
+++ header.py
@@ -1,6 +1,7 @@
 import dash_bootstrap_components as dbc
 from dash import html
 from dash_bootstrap_templates import ThemeSwitchAIO
+from dash import dcc
 
 icons = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
 
@@ -49,4 +50,77 @@ def header():
             ),
         ],
         className="mt-3",
+    )
+
+
+def filter():
+    return dbc.Row(
+        dbc.Col(
+            dbc.Card(
+                [
+                    dbc.CardHeader("Filters", className="border-0 bg-transparent"),
+                    dbc.CardBody(
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    dbc.Checklist(
+                                        id="borough-checklist",
+                                        options=[
+                                            {"label": b, "value": b.upper()}
+                                            for b in [
+                                                "Queens",
+                                                "Brooklyn",
+                                                "Manhattan",
+                                                "Bronx",
+                                                "Staten Island",
+                                            ]
+                                        ],
+                                        value=[
+                                            "QUEENS",
+                                            "BROOKLYN",
+                                            "MANHATTAN",
+                                            "BRONX",
+                                            "STATEN ISLAND",
+                                        ],
+                                        inline=True,
+                                        switch=True,
+                                    ),
+                                    md=6,
+                                    xs=12,
+                                ),
+                                dbc.Col(
+                                    dcc.RangeSlider(
+                                        id="year-range",
+                                        min=2018,
+                                        max=2026,
+                                        step=1,
+                                        value=[2018, 2026],
+                                        marks={
+                                            year: str(year) if year % 2 == 0 else ""
+                                            for year in range(2018, 2027)
+                                        },
+                                        tooltip={
+                                            "placement": "bottom",
+                                            "always_visible": True,
+                                            "style": {"color": "white"},
+                                        },
+                                        allowCross=False,
+                                        updatemode="mouseup",
+                                        pushable=1,
+                                        included=True,
+                                    ),
+                                    md=6,
+                                    xs=12,
+                                ),
+                            ],
+                            className="gy-2",
+                        )
+                    ),
+                ],
+                className="border-0 bg-transparent",
+            ),
+            width=10,
+        ),
+        justify="center",
+        className="shadow-sm",
     )
