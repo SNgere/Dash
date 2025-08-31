92fd11b8c7ca3c8ab1a80d63ddc1eaccdcf0bc08
vice <vice@vice.com>
2025-08-31 21:06:34 +0300

    add word cloud

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from header import header, filter
 from kpi import kpi
 import duckdb
 import plotly.express as px
+from wordcloud import WordCloud
+from io import BytesIO
+import base64
 
 
 con = duckdb.connect("crashes.duckdb", read_only=True)
@@ -36,6 +39,40 @@ WHERE BOROUGH IN ? AND YEAR BETWEEN ? AND ?
 GROUP BY BOROUGH,YEAR
 ORDER BY BOROUGH, YEAR;
 """
+
+
+word_query = """
+    WITH factors AS (
+    SELECT BOROUGH, Year, "CONTRIBUTING FACTOR VEHICLE 1" AS reasons
+    FROM crashes
+    UNION ALL
+    SELECT BOROUGH, Year, "CONTRIBUTING FACTOR VEHICLE 2"
+    FROM crashes
+    UNION ALL
+    SELECT BOROUGH, Year, "CONTRIBUTING FACTOR VEHICLE 3"
+    FROM crashes
+    UNION ALL
+    SELECT BOROUGH, Year, "CONTRIBUTING FACTOR VEHICLE 4"
+    FROM crashes
+    UNION ALL
+    SELECT BOROUGH Year, "CONTRIBUTING FACTOR VEHICLE 5"
+    FROM crashes
+),
+reasons AS (
+    SELECT Year, reasons
+    FROM factors
+    WHERE BOROUGH IN ? AND YEAR BETWEEN ? AND ?
+),
+words AS (
+    SELECT reasons, COUNT(reasons) AS Count
+    FROM reasons
+    GROUP BY reasons
+    
+) 
+SELECT * 
+FROM words
+ORDER BY Count DESC
+ """
 
 
 def line_chart(borough, year_range):
@@ -71,6 +108,35 @@ def line_chart(borough, year_range):
     return fig
 
 
+def word_cloud_plot(worddf):
+    word_dict = dict(zip(worddf["Word"], worddf["Count"]))
+    wordcloud = WordCloud(
+        # width=1200,
+        # height=590,
+        background_color=None,
+        mode="RGBA",
+        colormap="YlOrRd",
+    ).generate_from_frequencies(word_dict)
+    return wordcloud
+
+
+def word_cloud_func(borough, year_range):
+    start_year, end_year = year_range
+
+    if not borough:
+        borough = ["QUEENS", "BROOKLYN", "MANHATTAN", "BRONX", "STATEN ISLAND"]
+
+    params = (borough, start_year, end_year)
+
+    worddf = con.execute(word_query, parameters=params).df()
+    wordcloud = word_cloud_plot(worddf)
+    buffer = BytesIO()
+    wordcloud.to_image().save(buffer, format="PNG")
+    img_base64 = base64.b64encode(buffer.getvalue()).decode()
+
+    return f"data:image/png;base64,{img_base64}"
+
+
 @app.callback(
     Output("content", "children"),
     [
@@ -82,38 +148,68 @@ def update_app(borough, year_range):
     return [
         kpi(con, borough, year_range),
         dbc.Row(
-            children=dbc.Col(
-                children=[
-                    dbc.Card(
-                        children=[
-                            dbc.CardHeader(
-                                "Crashes by Borough Over Time", className="fw-bold border-0"
-                            ),
-                            dbc.CardBody(
-                                children=[
-                                    dcc.Graph(
-                                        figure=line_chart(borough, year_range),
-                                        config={
-                                            "displayModeBar": False,
-                                            "staticPlot": True,
-                                        },
-                                    )
-                                ],
-                                className="border-0"
-                            ),
-                            dbc.CardFooter(
-                                "Yearly trends of vehicle collisions across selected boroughs",
-                                className="fw-bold border-0",
-                            ),
-                        ],
-                        className="border-0 shadow"
-                    )
-                ],
-                xs=12,
-                md=7
-            ),
+            children=[
+                dbc.Col(
+                    children=[
+                        dbc.Card(
+                            children=[
+                                dbc.CardHeader(
+                                    "Crashes by Borough Over Time",
+                                    className="fw-bold border-0",
+                                ),
+                                dbc.CardBody(
+                                    children=[
+                                        dcc.Graph(
+                                            figure=line_chart(borough, year_range),
+                                            config={
+                                                "displayModeBar": False,
+                                                "staticPlot": True,
+                                            },
+                                        )
+                                    ],
+                                    className="border-0",
+                                ),
+                                dbc.CardFooter(
+                                    "Yearly trends of vehicle collisions across selected boroughs",
+                                    className="fw-bold border-0",
+                                ),
+                            ],
+                            className="border-0 shadow",
+                        )
+                    ],
+                    xs=12,
+                    md=7,
+                ),
+                dbc.Col(
+                    children=[
+                        dbc.Card(
+                            children=[
+                                dbc.CardHeader(
+                                    "Crashes by Borough Over Time",
+                                    className="fw-bold border-0",
+                                ),
+                                dbc.CardBody(
+                                    children=[
+                                        html.Img(
+                                            src=word_cloud_func(borough, year_range)
+                                        )
+                                    ],
+                                    className="border-0",
+                                ),
+                                dbc.CardFooter(
+                                    "Yearly trends of vehicle collisions across selected boroughs",
+                                    className="fw-bold border-0",
+                                ),
+                            ],
+                            className="border-0 shadow",
+                        )
+                    ],
+                    xs=12,
+                    md=3,
+                ),
+            ],
             className="mt-3",
-            justify="around"
+            justify="around",
         ),
     ]
 
