c349a9d2dac0b43a96f8deaaa608ad3951bf956c
vice <vice@vice.com>
2025-09-01 18:23:21 +0300

    draft 1

--- row3.py
+++ row3.py
@@ -2,49 +2,6 @@ import dash_bootstrap_components as dbc
 from dash import dcc
 import plotly.express as px
 
-line_query = """
-SELECT 
-    BOROUGH,
-    YEAR AS Year,
-    COUNT(*) AS total_collisions
-FROM crashes
-WHERE BOROUGH IN ? AND YEAR BETWEEN ? AND ?
-GROUP BY BOROUGH,YEAR
-ORDER BY BOROUGH, YEAR;
-"""
-
-bar_query = """
-WITH all_vehicles AS (
-    SELECT BOROUGH, Year, "VEHICLE TYPE CODE 1" AS Vehicles
-    FROM crashes
-    WHERE "VEHICLE TYPE CODE 1" IS NOT NULL AND "VEHICLE TYPE CODE 1" <> ''
-    UNION ALL
-    SELECT BOROUGH, Year, "VEHICLE TYPE CODE 2" AS Vehicles
-    FROM crashes
-    WHERE "VEHICLE TYPE CODE 2" IS NOT NULL AND "VEHICLE TYPE CODE 2" <> ''
-    UNION ALL
-    SELECT BOROUGH, Year, "VEHICLE TYPE CODE 3" AS Vehicles
-    FROM crashes
-    WHERE "VEHICLE TYPE CODE 3" IS NOT NULL AND "VEHICLE TYPE CODE 3" <> ''
-    UNION ALL
-    SELECT BOROUGH, Year, "VEHICLE TYPE CODE 4" AS Vehicles
-    FROM crashes
-    WHERE "VEHICLE TYPE CODE 4" IS NOT NULL AND "VEHICLE TYPE CODE 4" <> ''
-    UNION ALL
-    SELECT BOROUGH, Year, "VEHICLE TYPE CODE 5" AS Vehicles
-    FROM crashes
-    WHERE "VEHICLE TYPE CODE 5" IS NOT NULL AND "VEHICLE TYPE CODE 5" <> ''
-)
-SELECT
-    Vehicles,
-    COUNT(*) AS count
-FROM all_vehicles
-WHERE BOROUGH IN ? AND YEAR BETWEEN ? AND ?
-GROUP BY Vehicles
-ORDER BY count DESC
-LIMIT 6;
-"""
-
 
 map_query = """
 SELECT
@@ -129,7 +86,7 @@ def row3(borough, year_range, template, con):
                             ),
                             dbc.CardFooter(
                                 "Tip: You can switch layers on and off by clicking items in the legend",
-                                className="fw-bold border-0 text-center font-italic",
+                                className="fw-bold border-0 text-center font-italic mt-1",
                             ),
                         ],
                         className="border-0 shadow-lg",
